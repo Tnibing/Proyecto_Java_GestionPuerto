21edec410a23b47aa9b68b14b0eb80f51e2cabfe
Tnibing <frnaad@gmail.com>
2024-04-17 20:19:45 +0200

    Prueba

--- src/agentes/GUI_TEST.java
+++ src/agentes/GUI_TEST.java
@@ -2,13 +2,14 @@ package agentes;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 /**
@@ -22,6 +23,7 @@ public class GUI_TEST extends JFrame implements ActionListener {
     private JComboBox listaPuertos;
     private JButton boton;
     private JTextArea info;
+    private JPanel texto;
     
     public GUI_TEST() {
         
@@ -32,8 +34,15 @@ public class GUI_TEST extends JFrame implements ActionListener {
         
         boton = new JButton("Aceptar");
         
-        info = new JTextArea();
+        texto = new JPanel();
+        texto.setPreferredSize(new Dimension(65, 65));
+        texto.setBackground(new Color(0x649EFF));
+        
+        info = new JTextArea("HOLA");
         info.setEditable(false);
+        info.setBackground(Color.red);
+        
+        texto.add(info);
         
         ImageIcon icono = new ImageIcon("buqueIcono.png");
         this.setIconImage(icono.getImage());
@@ -46,8 +55,7 @@ public class GUI_TEST extends JFrame implements ActionListener {
         listaPuertos = new JComboBox(puertos);
         */
         
-        this.add(boton, BorderLayout.EAST);
-        this.add(new JScrollPane(info), BorderLayout.CENTER);
+        this.add(texto, BorderLayout.CENTER);
         //this.add(listaNavieras, BorderLayout.NORTH);
         //this.add(listaPuertos);
         this.setSize(1400, 800);
