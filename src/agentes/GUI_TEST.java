ba42273fcd755415da30327418911acdef92b46b
Tnibing <frnaad@gmail.com>
2024-04-17 17:52:11 +0200

    Un poco de GUI

--- src/agentes/GUI_TEST.java
+++ src/agentes/GUI_TEST.java
@@ -1,10 +1,10 @@
 package agentes;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
+import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
@@ -12,24 +12,32 @@ import javax.swing.JFrame;
  *
  * @author Fran <6W>
  */
+
 public class GUI_TEST extends JFrame implements ActionListener {
     
-    JButton boton;
-    JCheckBox seleccion;
+    JComboBox listaNavieras;
+    JComboBox listaPuertos;
     
     public GUI_TEST() {
+        
+        this.setTitle("Gestión Portuaria");
+        this.getContentPane().setBackground(new Color(0x649EFF));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
         
+        ImageIcon icono = new ImageIcon("buqueIcono.png");
+        this.setIconImage(icono.getImage());
+        
         String[] navieras = {"Arkas", "Maersk", "CMA"};
-        JComboBox listaNavieras = new JComboBox(navieras);
+        listaNavieras = new JComboBox(navieras);
         
         String[] puertos = {"Valencia (España)", "Castellón (España)", "Barcelona (España)", "Pireo (Grecia)", "Estambul (Turquía)", "Yarimca (Turquía)"};
-        JComboBox listaPuertos = new JComboBox(puertos);
+        listaPuertos = new JComboBox(puertos);
         
         this.add(listaNavieras);
         this.add(listaPuertos);
-        this.pack();
+        this.setSize(1400, 800);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
     
--- src/agentes/Main.java
+++ src/agentes/Main.java
@@ -11,6 +11,5 @@ public class Main {
         // TEST
         new GUI_TEST();
         
-  
     }
 }
