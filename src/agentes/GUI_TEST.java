b401c0f5c644245573d2728aa83f58842f6d6ba8
Tnibing <frnaad@gmail.com>
2024-04-18 22:49:02 +0200

    Pruebas de GUI constraints para componentes

--- src/agentes/GUI_TEST.java
+++ src/agentes/GUI_TEST.java
@@ -3,6 +3,10 @@ package agentes;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
@@ -16,9 +20,8 @@ import javax.swing.JTextArea;
  *
  * @author Fran <6W>
  */
-
 public class GUI_TEST extends JFrame implements ActionListener {
-    
+
     private JComboBox listaNavieras;
     private JComboBox listaPuertos;
     private JButton boton;
@@ -26,45 +29,66 @@ public class GUI_TEST extends JFrame implements ActionListener {
     private JPanel texto;
     private JPanel ladoIzquierdo;
     private JPanel centro;
-    
+    private GridBagConstraints constrainsListaPuertos;
+    private GridBagConstraints constrainsListaNavieras;
+
     public GUI_TEST() {
-        
+
         this.setTitle("Gestión Portuaria");
-        this.getContentPane().setBackground(new Color(0x649EFF));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
-        
+
         boton = new JButton("Aceptar");
-        
+
         centro = new JPanel();
         centro.setBackground(new Color(0x649EFF));
         centro.add(boton);
-        
+
         texto = new JPanel();
         texto.setPreferredSize(new Dimension(65, 65));
         texto.setBackground(new Color(0x649EFF));
-        
-        ladoIzquierdo = new JPanel();
-        
+
         info = new JTextArea("HOLA");
         info.setEditable(false);
         info.setBackground(Color.red);
-        
+
         texto.add(info);
-        
+
         ImageIcon icono = new ImageIcon("buqueIcono.png");
         this.setIconImage(icono.getImage());
-        
-        
+
         String[] navieras = {"Arkas", "Maersk", "CMA"};
         listaNavieras = new JComboBox(navieras);
-        
+        listaNavieras.setPreferredSize(new Dimension(139, 25));
+
         String[] puertos = {"Valencia (España)", "Castellón (España)", "Barcelona (España)", "Pireo (Grecia)", "Estambul (Turquía)", "Yarimca (Turquía)"};
         listaPuertos = new JComboBox(puertos);
+
+        ladoIzquierdo = new JPanel();
+        ladoIzquierdo.setLayout(new GridBagLayout());
         
+        constrainsListaPuertos = new GridBagConstraints();
+        constrainsListaPuertos.gridx  = 0;
+        constrainsListaPuertos.gridy = 0;
+        constrainsListaPuertos.gridwidth = 1;
+        constrainsListaPuertos.gridheight = 1;
+        constrainsListaPuertos.anchor = GridBagConstraints.NORTH;
+        constrainsListaPuertos.insets = new Insets(5, 5, 5, 5);
+        constrainsListaPuertos.weighty = 0;
+
+        constrainsListaNavieras = new GridBagConstraints();
+        constrainsListaNavieras.gridx  = 0;
+        constrainsListaNavieras.gridy = 1;
+        constrainsListaNavieras.gridwidth = 1;
+        constrainsListaNavieras.gridheight = 1;
+        constrainsListaNavieras.anchor = GridBagConstraints.NORTH;
+        constrainsListaNavieras.insets = new Insets(5, 5, 5, 5);
+        constrainsListaNavieras.weighty = 1;
+        
+        ladoIzquierdo.setPreferredSize(new Dimension(290, 800));
         ladoIzquierdo.setBackground(Color.WHITE);
-        ladoIzquierdo.add(listaPuertos);
-        ladoIzquierdo.add(listaNavieras);
+        ladoIzquierdo.add(listaPuertos, constrainsListaPuertos);
+        ladoIzquierdo.add(listaNavieras, constrainsListaNavieras);
 
         this.add(texto, BorderLayout.CENTER);
         this.add(ladoIzquierdo, BorderLayout.WEST);
@@ -73,9 +97,9 @@ public class GUI_TEST extends JFrame implements ActionListener {
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
     }
 }
