3ea421569592cd26e0a94799363938cce79b1add
Tnibing <frnaad@gmail.com>
2024-04-24 23:37:12 +0200

    Arreglo del bug de modificación concurrente

--- src/controladoresEventos/EventoBotonComenzarPararRuta.java
+++ src/controladoresEventos/EventoBotonComenzarPararRuta.java
@@ -5,6 +5,8 @@ import cargas.Contenedor;
 import gui.GUI;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -61,6 +63,8 @@ public class EventoBotonComenzarPararRuta implements ActionListener {
 
                     }
                 });
+                
+                HiloBuqueArkas.setName("Hilo buque arkas");
 
                 // bucle del buque de Maersk
                 // usando lambda expression por practicar
@@ -75,6 +79,8 @@ public class EventoBotonComenzarPararRuta implements ActionListener {
                     
                     }
                 });
+                
+                HiloBuqueMaersk.setName("Hilo buque maersk");
 
                 // bucle del buque de CMA
                 HiloBuqueCMA = new Thread(new Runnable() {
@@ -88,6 +94,8 @@ public class EventoBotonComenzarPararRuta implements ActionListener {
 
                     }
                 });
+                
+                HiloBuqueCMA.setName("Hilo buque cma");
 
                 // Se inician los 3 hilos
                 HiloBuqueArkas.start();
@@ -117,6 +125,7 @@ public class EventoBotonComenzarPararRuta implements ActionListener {
 
         });
 
+        HiloGeneralBuques.setName("Hilo general gestión buques");
         HiloGeneralBuques.start();
     }
 
@@ -133,35 +142,52 @@ public class EventoBotonComenzarPararRuta implements ActionListener {
             }
 
             // Descarga de contenedores
-            List<Contenedor> contenedoresBuque = b.getContenedores();
+            // Copia de la lista de contenedores que hay en el buque que se tendrán que eliminar y pasar al puerto
+            List<Contenedor> contenedoresBuque = new ArrayList<>(List.copyOf(b.getContenedores()));
+            
+           // Lista de los contenedores que se descargan, hay que meterlos al puerto y eliminarlos del buque
+            List <Contenedor> contenedoresDescargados = new ArrayList<>();
+            
+            Iterator<Contenedor> iteradorContenedores = contenedoresBuque.iterator();
 
-            int contenedoresDescargados = 0;
-
-            for (Contenedor c : contenedoresBuque) {
+            while (iteradorContenedores.hasNext()) {
+                
+                Contenedor c = iteradorContenedores.next();
 
                 if (c.getDestino() == b.getPuertoActual()) {
 
                     b.getPuertoActual().addContenedores(c);
-                    b.descargarContenedores(c);
-                    contenedoresDescargados++;
+                    contenedoresDescargados.add(c);
                 }
-
             }
+            
+            // Se eliminan del buque los contenedores que se han descargado en el puerto
+            b.getContenedores().removeAll(contenedoresDescargados);
 
-            texto.append("\nEl buque " + b.getNombre() + " ha DESCARGADO " + contenedoresDescargados + " contenedores en el puerto de " + b.getPuertoActual().getNombre() + ".\n");
+            texto.append("\nEl buque " + b.getNombre() + " ha DESCARGADO " + contenedoresDescargados.size() + " contenedores en el puerto de " + b.getPuertoActual().getNombre() + ".\n");
             // Fin descarga contenedores
 
             // Carga contenedores
-            List<Contenedor> contenedores = b.getPuertoActual().getContenedores();
-
-            for (Contenedor c : contenedores) {
-
+            // Lista de copia por que si no me sale un error de concurrencia al modificar una colección mientras la estoy iterando en otro proceso
+            List<Contenedor> contenedores = new ArrayList<>(List.copyOf(b.getPuertoActual().getContenedores()));
+            
+            //Aquí se guardan los contenedores que se cargan al buque y hay que eliminar del puerto
+            List<Contenedor> contenedoresCargados = new ArrayList<>();
+            
+            Iterator<Contenedor> iteradorContenedoresCargados = contenedores.iterator();
+            
+            while (iteradorContenedoresCargados.hasNext()) {
+                
+                Contenedor c = iteradorContenedoresCargados.next();
+                
                 if (c.getNaviera().getNombre().equals(b.getNombreNaviera()) && b.getContenedores().size() < b.getCapacidad() && !c.getDestino().toString().equals(b.getPuertoActual().toString())) {
                     b.addContenedor(c);
-                    b.getPuertoActual().cargarContenedores(c);
+                    contenedoresCargados.add(c);
                 }
-
             }
+            
+            // Elimino del puerto los contenedores que se han cargado al buque
+            b.getPuertoActual().getContenedores().removeAll(contenedoresCargados);
 
             // Saco el índice del puerto actual, a partir de ese índice, saco cuál sería el siguiente puerto de la lista
             int puertoActual = b.getRuta().getPuertos().indexOf(b.getPuertoActual());
@@ -181,7 +207,7 @@ public class EventoBotonComenzarPararRuta implements ActionListener {
             
             b.setPuertoActual(siguientePuerto);
 
-            texto.append("El buque " + b.getNombre() + " ha CARGADO " + b.getContenedores().size() + " contenedores en el puerto de " + puerto.getNombre() + ".\n"
+            texto.append("El buque " + b.getNombre() + " ha CARGADO " + contenedoresCargados.size() + " contenedores en el puerto de " + puerto.getNombre() + ".\n"
                     + "Siguiente destino:\n" + siguientePuerto.infoPuerto() + "\n");
 
             // Fin carga contenedores
